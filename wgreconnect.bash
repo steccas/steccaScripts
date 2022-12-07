2d50acb3bc2596d2a8da117d12b2c95a56cda3b3
Luca Steccanella <steccas@pm.me>
2022-12-07 13:05:00 +0100

    Update wgreconnect.bash

--- wgreconnect.bash
+++ wgreconnect.bash
@@ -1,10 +1,17 @@
 #!/bin/bash
+
+if [ -z "$1" ] || [ -z "$2" ]
+  then
+    echo "Supply wg server ip and wg device"
+    exit 1
+fi
+
 while true
 do
     sleep 15
-    ping -c 1 <your_wireguard_server_ip>
+    ping -c 1 $1
     if [ $? != 0 ]
     then
-      sudo systemctl restart wg-quick@wg0 #change wg device if needed
+      sudo systemctl restart wg-quick@$2
     fi
 done
