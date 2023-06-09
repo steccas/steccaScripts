46a682392518531436bb80718eed038295dde1c7
Luca Steccanella <steccas@pm.me>
2023-06-09 13:13:03 +0200

    Update zshsetup.sh

--- zshsetup.sh
+++ zshsetup.sh
@@ -3,6 +3,7 @@ echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-autos
 curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-autosuggestions.gpg > /dev/null
 sudo apt update
 sudo apt install zsh zsh-autosuggestions zsh-syntax-highlighting nano
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
