c7e58edd3561cc6daa7152948b235ece226eda4a
Luca Steccanella <steccas@pm.me>
2023-06-09 13:08:24 +0200

    Create zshsetup.sh

---
+++ zshsetup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-autosuggestions/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-autosuggestions.list
+curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-autosuggestions.gpg > /dev/null
+sudo apt update
+sudo apt install zsh zsh-autosuggestions zsh-syntax-highlighting nano
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "set ZSH_THEME="powerlevel10k/powerlevel10k" and plugins=(git git-prompt git-lfs github gitignore gnu-utils zsh-autosuggestions history-substring-search zsh-completions docker docker-compose copypath copyfile colored-man-pages colorize man screen systemd themes ufw zsh-interactive-cd zsh-syntax-highlighting)"
+nano .zshrc
+exit 0
