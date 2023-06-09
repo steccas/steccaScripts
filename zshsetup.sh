3e9463c6649bb5da45ead6bf25b8d7e5b623dba9
Luca Steccanella <steccas@pm.me>
2023-06-09 13:10:34 +0200

    Update zshsetup.sh

--- zshsetup.sh
+++ zshsetup.sh
@@ -6,6 +6,6 @@ sudo apt install zsh zsh-autosuggestions zsh-syntax-highlighting nano
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "set ZSH_THEME="powerlevel10k/powerlevel10k" and plugins=(git git-prompt git-lfs github gitignore gnu-utils zsh-autosuggestions history-substring-search zsh-completions docker docker-compose copypath copyfile colored-man-pages colorize man screen systemd themes ufw zsh-interactive-cd zsh-syntax-highlighting)"
+echo "set ZSH_THEME=\"powerlevel10k/powerlevel10k\" and plugins=(git git-prompt git-lfs github gitignore gnu-utils zsh-autosuggestions history-substring-search zsh-completions docker docker-compose copypath copyfile colored-man-pages colorize man screen systemd themes ufw zsh-interactive-cd zsh-syntax-highlighting)"
 nano .zshrc
 exit 0
