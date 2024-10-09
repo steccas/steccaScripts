b33e3927a37e4d7c1d8ae00002cf8ff7a648eece
Luca Steccanella <steccas@pm.me>
2024-10-09 17:52:18 +0200

    Update zshsetup.sh

--- zshsetup.sh
+++ zshsetup.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-autosuggestions/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-autosuggestions.list
-curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-autosuggestions.gpg > /dev/null
 sudo apt update
-sudo apt install zsh zsh-autosuggestions zsh-syntax-highlighting nano
+sudo apt install zsh nano
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "set ZSH_THEME=\"powerlevel10k/powerlevel10k\" and plugins=(git git-prompt git-lfs github gitignore gnu-utils zsh-autosuggestions history-substring-search zsh-completions docker docker-compose copypath copyfile colored-man-pages colorize man screen systemd themes ufw zsh-interactive-cd zsh-syntax-highlighting)"
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+echo "set ZSH_THEME=\"powerlevel10k/powerlevel10k\" and plugins=(git git-prompt git-lfs github gitignore gnu-utils zsh-autosuggestions history-substring-search zsh-history-substring-search zsh-completions docker docker-compose copypath copyfile colored-man-pages colorize man screen systemd themes ufw zsh-interactive-cd zsh-syntax-highlighting)"
+echo "read the script comments"
+#Add it to FPATH in your .zshrc by adding the following line before source "$ZSH/oh-my-zsh.sh":
+#fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
+
+echo "Press any key to proceed..."
+
+# Loop until a key is pressed
+while true; do
+read -rsn1 key  # Read a single character silently
+if [[ -n "$key" ]]; then
 nano .zshrc
 exit 0
